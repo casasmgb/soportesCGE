3e4c1f2617a3d5cef7efc50610b7d97cef10078c
casasmgb <casasmgb@gmail.com>
2018-10-19 10:44:44 -0400

    error verificar la funcion generica para crear tabla

--- SSCRRHH migrar personas de db_personal a SSCRRHH.sql
+++ SSCRRHH migrar personas de db_personal a SSCRRHH.sql
@@ -16,13 +16,17 @@ CREATE PROCEDURE [dbo].[spr_creat_personal_backup_SSCRRHH] @nombre varchar(20)
 AS
 BEGIN
 	DECLARE @nombreTabla NVARCHAR(200)
+	DECLARE @sqlQuery NVARCHAR(8000)
+	DECLARE @sqlVerify NVARCHAR(100)
+	
 	SET @nombreTabla = @nombre
-	SELECT	per.per_Item, 
+	SET @sqlVerify = 'SELECT * FROM ' + @nombreTabla
+	SET sqlQuery = 'SELECT	per.per_Item, 
 			per.per_Tarjeta, 
 			per.per_DocIdentidad,
 			per.per_Paterno, 
 			per.per_Materno, 
-			per.per_PrimerNombre + ' ' +per.per_SegundoNombre as per_nombres, 
+			per.per_PrimerNombre + '' '' +per.per_SegundoNombre as per_nombres, 
 			car.car_Descripcion,
 			prof.pro_Descripcion,
 			uni.uni_Descripcion,
@@ -36,21 +40,22 @@ BEGIN
 			GETDATE() as export,
 			per.per_Piso,
 			per.per_Interno,
-			'00:00' as CH,
-			'00:00' as CH2,
-			'B3' as G
-	into @nombreTabla
-	FROM BDPersonal_dev.dbo.Persona per 
+			''00:00'' as CH,
+			''00:00'' as CH2,
+			''B3'' as G
+	into ' + @nombreTabla +' FROM BDPersonal_dev.dbo.Persona per 
 	INNER JOIN BDPersonal_dev.dbo.Cargo car on car.car_Codigo = per.car_Codigo
 	INNER JOIN BDPersonal_dev.dbo.Profesion prof on prof.pro_Codigo = per.pro_Codigo
 	INNER JOIN BDPersonal_dev.dbo.Unidad uni on uni.uni_Codigo = per.uni_Codigo
 	INNER JOIN BDPersonal_dev.dbo.Residencia res on res.res_Codigo = per.res_Codigo
 	INNER JOIN BDPersonal_dev.dbo.Escala esc on esc.esc_Codigo = per.esc_Codigo
 	INNER JOIN BDPersonal_dev.dbo.MovilidadFuncional mov_fun on mov_fun.per_Codigo = per.per_Codigo
-	where mov_fun.Vigente_MovilidadFuncional = 1 and per.per_Vigente = 1;
-
+	where mov_fun.Vigente_MovilidadFuncional = 1 and per.per_Vigente = 1'
+	
+	EXEC @sqlQuery 
+	
 	-- select * from personal_2018_octubre
-	IF NOT EXISTS (select * from @nombreTabla)
+	IF NOT EXISTS ( EXEC @sqlVerify )
 		BEGIN
 		    RAISERROR (
 		        'NO SE PUDO CREAR LA TABLA',
@@ -60,3 +65,6 @@ BEGIN
 		    GOTO HANDLE_ERROR 
 		END	
 END 
+
+-- DROP TABLE personal_2018_octubre
+-- select * from personal_2018_octubre
